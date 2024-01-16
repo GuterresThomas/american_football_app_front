f2b84322f384462e72bd0b23395d327dc09186b8
Guterresthomas <guterresmikael@gmail.com>
2024-01-16 15:00:12 -0300

    axios e logo

---
+++ src/components/homepage.tsx
@@ -0,0 +1,30 @@
+'use client'
+import axios from 'axios'
+
+export default function HomePageComponent() {
+    async function buscarJogos() {
+        const options = {
+            method: 'GET',
+            url: 'https://api-american-football.p.rapidapi.com/games',
+            params: {date: '2024-01-15'},
+            headers: {
+                'X-RapidAPI-Key': '1175bafbc2mshcbde2b5b68642f3p11ba34jsndd65f63e31f2',
+                'X-RapidAPI-Host': 'api-american-football.p.rapidapi.com'
+            }
+            };
+        
+            try {
+                const response = await axios.request(options);
+                console.log(response.data);
+            } catch (error) {
+                console.error(error);
+            }
+    }
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
